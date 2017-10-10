d43acd730fd450f21213d19620abf6d8daf73eae
phoebe jenkins <feeeb@protonmail.com>
2017-10-10 15:30:15 -0400

    Implement or8way.

---
+++ proj1/test_my_or8way.v
@@ -0,0 +1,35 @@
+`include "my_or8way.v"
+
+module test_my_or8way();
+   reg [8:0] a;
+   reg 	     expected;
+   wire      b;
+
+   my_or8way u1(b, a);
+   
+   initial 
+     begin
+	a = 8'b00000000;
+	expected = 0;
+
+	#1 
+	a = 8'b00000001;
+	expected = 1;
+
+	#1
+	a = 8'b00000000;
+	expected = 0;
+
+	#1 
+	a = 8'b00000010;
+	expected = 1;
+
+	#1 
+        a = 8'b11111111;
+	expected = 1;
+     end
+
+   initial
+     $monitor("my_or8way %d %b (%b %b)", $time, a, b, expected);
+   
+endmodule
